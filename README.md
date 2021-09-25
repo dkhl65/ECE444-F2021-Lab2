0998b0c3154f6dfa3a25fc006528b274987e9c15
Daniel Liang <daniel65@live.ca>
2021-09-25 17:07:10 -0400

    Activity 4 steps 2 to 5: add email to form and display UofT email address.

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ Activity 2: reproduction of example 2-2.
 Activity 3: chapter 3 examples of navigation bar, hello message and LLLL timestamp. 
 <br/>
 ![alt text](screenshots/Activity3.png?raw=true)
+<br/>
+<br/>
+Activity 4 step 2: add a field for filling in email address. 
+<br/>
+![alt text](screenshots/Activity4-2.png?raw=true)
+<br/>
+<br/>
+Activity 4 step 3: first name and UofT email address submitted.
+<br/>
+![alt text](screenshots/Activity4-3.png?raw=true)
+<br/>
+<br/>
+Activity 4 step 4: first name and last name filled in with email error. 
+<br/>
+![alt text](screenshots/Activity4-4.png?raw=true)
+<br/>
+<br/>
+Activity 4 step 5: first name and last name and non-UofT email submitted. 
+<br/>
+![alt text](screenshots/Activity4-5.png?raw=true)
--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your U of T Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,11 +22,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 if __name__ == '__main__':
     app.run(debug=True)
